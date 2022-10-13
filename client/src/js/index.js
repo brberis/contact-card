2710bd6e73c16a62740bbec139ba7af0cc3dd685
Cristobal Barberis <cristobal@barberis.com>
2022-10-13 11:27:29 -0400

    put form

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,5 +1,5 @@
 import  "./form";
-import "./submit";
+// import "./submit";
 // Import CSS files
 import "../css/index.css";
 import { Tooltip, Toast, Popover } from 'bootstrap';
@@ -8,10 +8,51 @@ import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
 import { initdb } from './database';
+import { fetchCards } from './cards';
+import { toggleForm, clearForm } from './form';
+
+
 
 window.addEventListener('load', function () {
   initdb();
+  fetchCards();
   document.getElementById('logo').src = Logo;
   document.getElementById('bearThumbnail').src = Bear;
   document.getElementById('dogThumbnail').src = Dog;
 });
+
+  // Form functionality
+  const form = document.getElementById("formToggle");
+  const newContactButton = document.getElementById("new-contact");
+  let submitBtnToUpdate = false;
+  let profileId;
+  
+  newContactButton.addEventListener('click', event => {
+    toggleForm()
+   })
+  
+  form.addEventListener('submit', event => {
+    // Handle data
+    event.preventDefault();
+    let name = document.getElementById("name").value;
+    let phone = document.getElementById("phone").value;
+    let email = document.getElementById("email").value;
+    let profile = document.querySelector('input[type="radio"]:checked').value;
+    
+    // Post form data to IndexedDB OR Edit an existing card in IndexedDB
+    if (submitBtnToUpdate == false) {
+      postDb(name, email, phone, profile);
+    } else {
+  
+    fetchCards();
+    // Toggles the submit button back to POST functionality
+    submitBtnToUpdate = false;
+  }
+  
+  // Clear form
+  clearForm();
+  // Toggle form
+  toggleForm();
+  // Reload the DOM
+  fetchCards();
+  });
